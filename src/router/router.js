97cef7da60a451aa4ba1ae9eac3bace7ea17bd02
蟑螂恶霸 <zhanglangeba@qq.com>
2020-01-05 16:47:39 +0800

    项目结构的搭建及路由匹配

---
+++ src/router/router.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/views/home.vue'
+import Air from '@/views/air/index.vue'
+import Hotel from '@/views/hotel/index.vue'
+import Post from '@/views/post/index.vue'
+import User from '@/views/user/index.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    routes: [
+        {
+            name: 'home',
+            path: '/home',
+            component: Home
+        },
+        {
+            name: 'air',
+            path: '/air',
+            component: Air
+        },
+        {
+            name: 'hotel',
+            path: '/hotel',
+            component: Hotel
+        },
+        {
+            name: 'post',
+            path: '/post',
+            component: Post
+        },
+        {
+            name: 'user',
+            path: '/user',
+            component: User
+        },
+    ]
+})
