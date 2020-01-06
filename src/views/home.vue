5feb145e547452ad5924517a0304681c76a81ba8
蟑螂恶霸 <zhanglangeba@qq.com>
2020-01-06 17:24:48 +0800

    首页渲染完毕

--- src/views/home.vue
+++ src/views/home.vue
@@ -12,6 +12,29 @@
         ></div>
       </el-carousel-item>
     </el-carousel>
+
+    <!-- 搜索框 -->
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 -->
+        <el-row type="flex" class="search-tab">
+          <span
+            v-for="(item,index) in option"
+            :key="index"
+            @click="handleClick(index)"
+            :class="{active: currentIndex === index}"
+          >
+            <i>{{item.title}}</i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input :placeholder="option[currentIndex].placeholder" />
+          <i class="el-icon-search"></i>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,18 +42,41 @@
 export default {
   data() {
     return {
+      // 当前索引
+      currentIndex: 0,
       // 轮播图数据
-      banners: []
+      banners: [],
+      // 定义tap搜索框数据
+      option: [
+        {
+          title: "攻略",
+          placeholder: "请输入城市"
+        },
+        {
+          title: "酒店",
+          placeholder: "请输入城市搜索酒店"
+        },
+        {
+          title: "机票",
+          placeholder: ""
+        }
+      ]
     };
   },
   mounted() {
     // 获取轮播图数据
     this.$axios({ url: "/scenics/banners" }).then(res => {
-      console.log(res);
-      this.banners = res.data.data
+      this.banners = res.data.data;
       // console.dir(this.$axios);
-      
     });
+  },
+  methods: {
+    handleClick(index) {
+      this.currentIndex = index;
+      if(index === 2){
+        this.$router.push({name: 'air'})
+      }
+    }
   }
 };
 </script>
@@ -48,6 +94,95 @@ export default {
   .banner-image {
     width: 100%;
     height: 100%;
+  }
+
+  .banner-content {
+    z-index: 9;
+    width: 1000px;
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    margin-left: -500px;
+    border-top: 1px transparent solid;
+
+    .search-bar {
+      width: 552px;
+      margin: 0 auto;
+    }
+
+    .search-tab {
+      .active {
+        i {
+          color: #333;
+        }
+        &::after {
+          background: #eee;
+        }
+      }
+
+      span {
+        width: 82px;
+        height: 36px;
+        display: block;
+        position: relative;
+        margin-right: 8px;
+        cursor: pointer;
+
+        i {
+          position: absolute;
+          z-index: 2;
+          display: block;
+          width: 100%;
+          height: 100%;
+          line-height: 30px;
+          text-align: center;
+          color: #fff;
+        }
+
+        &:after {
+          position: absolute;
+          left: 0;
+          top: 0;
+          display: block;
+          content: "";
+          width: 100%;
+          height: 100%;
+          border: 1px rgba(255, 255, 255, 0.2) solid;
+          border-bottom: none;
+          transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+          transform-origin: bottom left;
+          background: rgba(0, 0, 0, 0.5);
+          border-radius: 1px 2px 0 0;
+          box-sizing: border-box;
+        }
+      }
+    }
+
+    .search-input {
+      width: 550px;
+      height: 46px;
+      background: #fff;
+      border-radius: 0 4px 4px 4px;
+      border: 1px rgba(255, 255, 255, 0.2) solid;
+      border-top: none;
+      box-sizing: unset;
+
+      input {
+        flex: 1;
+        height: 20px;
+        padding: 13px 15px;
+        outline: none;
+        border: 0;
+        font-size: 16px;
+      }
+
+      .el-icon-search {
+        cursor: pointer;
+        font-size: 22px;
+        padding: 0 10px;
+        font-weight: bold;
+      }
+    }
   }
 }
 </style>
