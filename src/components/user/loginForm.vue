08144a02f419d90f2a84cdc71f8bdd8a484928bf
蟑螂恶霸 <zhanglangeba@qq.com>
2020-01-11 19:58:07 +0800

    登录授权

--- src/components/user/loginForm.vue
+++ src/components/user/loginForm.vue
@@ -49,7 +49,8 @@ export default {
             //  把用户数据存到vuex
              this.$store.commit('setUserInfo',res.data)
              this.$message.success('登录成功')
-             this.$router.push({ name: 'home'})
+            //  this.$router.push({ name: 'home'})
+             this.$router.back()
          })
         } else {
           return false;
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,20 +1,33 @@
 import axios from 'axios';
 import Vue from 'vue';
 import { Message } from 'element-ui'
+import router from '@/router/router'
+
 
 // 配置基准路径
 axios.defaults.baseURL = 'http://localhost:1337';
 
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
-    // 对响应数据做点什么
+    // 对响应数据做点什么 
     return response;
 }, function (error) {
     // 对响应错误做点什么
-    // console.log(error.response);
+    console.log(router);
+
     const { statusCode, message } = error.response.data
     if (statusCode === 400) {
         Message.error(message)
+
+    }
+    if (statusCode === 401 || statusCode === 403) {
+        Message.error("请先登录")
+        router.push({
+            name: 'login',
+        })
+
+
+
     }
 
     return Promise.reject(error);
